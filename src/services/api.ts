f668327ff35081beb0b3453d7e1f342231d7349e
MiguelNevesCamargoJunior <miguelnevescamargojunior@gmail.com>
2021-04-24 02:39:50 -0300

    ajustes no fake api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,5 @@
 import axios from 'axios';
+
 export const api = axios.create({
-//localhost fake api
-//baseURL: 'http://localhost:3333'
-
-//hosted fake api
-baseURL: 'https://my-json-server.typicode.com/MiguelNevesCamargoJunior/podcastrnext/'
-
+  baseURL: process.env.API_URL || 'http://localhost:3333/'
 })
