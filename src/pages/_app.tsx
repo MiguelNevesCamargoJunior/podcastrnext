257e4c729bae00c237e71602c60962196a66187e
MiguelNevesCamargoJunior <miguelnevescamargojunior@gmail.com>
2021-04-24 02:11:28 -0300

    final do nlw - app completo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,28 +3,15 @@ import '../styles/global.scss';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import styles from '../styles/app.module.scss';
-import { PlayerContext } from '../contexts/PlayerContext';
+
+import { PlayerContextProvider } from '../contexts/PlayerContext';
+
 
 function MyApp({ Component, pageProps }) {
-const [episodeList, setEpisodeList] = useState([]);
-const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-const [isPlaying,setIsPlaying] = useState(false);
-function play(episode) {
-  setEpisodeList([episode]);
-  setCurrentEpisodeIndex(0);
-  setIsPlaying(true);
-}
+return (
+<PlayerContextProvider>
 
-function togglePlay () {
-  setIsPlaying(!isPlaying);
-}
-
-function setPlayingState(state: boolean){
-  setIsPlaying(state);
-}
-  return (
-    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex, play, isPlaying, togglePlay,setPlayingState}}>
-    <div className={styles.wrapper}>
+<div className={styles.wrapper}>
       <main>
       <Header />
       <Component {...pageProps} />
@@ -32,7 +19,7 @@ function setPlayingState(state: boolean){
       <Player />
         
     </div>
-</PlayerContext.Provider>
+</PlayerContextProvider>
 ) 
 }
 
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,13 +2,14 @@ import { format, parseISO } from 'date-fns';
  
 import ptBR from 'date-fns/locale/pt-BR';
 import {GetStaticProps, GetStaticPaths} from 'next';
-
+import Head from 'next/head';
 import Link from 'next/link';
-import {useRouter} from 'next/router';
+ 
 import Image from "next/image";
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import styles from './episode.module.scss';
+import { usePlayer } from '../../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -28,13 +29,15 @@ type EpisodeProps = {
 }
 
 export default function Episode ({episode}: EpisodeProps ) {
- const router = useRouter();
- if(router.isFallback) {
-   return (<p>Carregando...</p>)
- }
+  const { play } = usePlayer();
+ 
   return (
     <div className={styles.episodeContainer}>
     <div className={styles.episode}>
+
+    <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/"> 
         <button type="button">
@@ -48,7 +51,7 @@ export default function Episode ({episode}: EpisodeProps ) {
             objectFit="cover"
           />
        
-        <button type="button">
+        <button type="button" onClick={()=>play(episode)}>
           <img src="/play.svg" alt="Tocar episódio" />
         </button>
         </div>
